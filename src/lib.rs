6f559ff92f447e8994772d79b9b4c32e022ff0a8
Nakayama Daiju <42.daiju@gmail.com>
2022-02-27 18:23:45 +0900

    dump date

--- src/lib.rs
+++ src/lib.rs
@@ -1,94 +1,144 @@
 #![allow(clippy::unusual_byte_groupings)]
 
-use std::thread;
+use std::{fmt::Write as FmtWrite, fs::File, io::Write, path::Path, thread};
 
-pub fn print_number_of_state<const M: usize, const E: usize>(number_of_state: [[u64; M]; E]) {
-    let mut width = [3; M];
-    print!("  \\m");
-    for (m, w) in width.iter_mut().enumerate() {
-        let mag = (m as i32 * 2) - M as i32 + 1;
-        for same_ene in number_of_state.iter() {
-            *w = (*w).max(same_ene[m].to_string().len());
-        }
-        print!(" {:d$}", mag, d = *w);
+#[derive(Debug)]
+pub struct NumberOfState<const M: usize, const E: usize> {
+    data: [[u64; M]; E],
+}
+
+impl<const M: usize, const E: usize> NumberOfState<M, E> {
+    pub fn new() -> NumberOfState<M, E> {
+        NumberOfState { data: [[0; M]; E] }
     }
-    println!();
-    print!(" e \\");
-    for &w in width.iter() {
-        print!("{}", "-".repeat(w + 1));
+
+    #[inline]
+    pub fn incr(&mut self, ene: u32, mag: u32) {
+        self.data[ene as usize][mag as usize] += 1;
     }
-    println!();
-    for (ene, same_ene) in number_of_state.iter().enumerate() {
-        if ene % 2 == 1 {
-            continue;
+
+    pub fn save_file<P: AsRef<Path>>(&self, path: P) -> std::io::Result<usize> {
+        let mut n = 0;
+        let mut file = File::create(path)?;
+        for (ene, same_ene) in self.data.iter().enumerate() {
+            let ene = ene as i32 * 2 - E as i32 + 1;
+            for (m, &c) in same_ene.iter().enumerate() {
+                let m = m as i32 * 2 - M as i32 + 1;
+                if c != 0 {
+                    writeln!(file, "{} {} {}", ene, m, c)?;
+                    n += 1;
+                }
+            }
         }
-        print!("{:3}|", ene as i32 * 2 - E as i32 + 1);
-        for (c, &w) in same_ene.iter().zip(width.iter()) {
-            print!(" {:w$}", c);
-        }
-        println!();
+        Ok(n)
     }
 }
 
-pub fn calc_2x2() -> [[u64; 5]; 9] {
-    let mut number_of_state = [[0; 5]; 9];
+impl<const M: usize, const E: usize> Default for NumberOfState<M, E> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<const M: usize, const E: usize> std::fmt::Display for NumberOfState<M, E> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut width = [3; M];
+        f.write_str("  \\m")?;
+        for (m, w) in width.iter_mut().enumerate() {
+            let mag = m as i32 * 2 - M as i32 + 1;
+            for same_ene in self.data.iter() {
+                *w = (*w).max(same_ene[m].to_string().len());
+            }
+            f.write_str(&format!(" {:d$}", mag, d = *w))?;
+        }
+        f.write_str("\n e \\")?;
+        for &w in width.iter() {
+            f.write_str("-".repeat(w + 1).as_str())?;
+        }
+        f.write_char('\n')?;
+        for (ene, same_ene) in self.data.iter().enumerate() {
+            if ene % 2 == 1 {
+                continue;
+            }
+            f.write_str(&format!("{:3}|", ene as i32 * 2 - E as i32 + 1))?;
+            for (c, &w) in same_ene.iter().zip(width.iter()) {
+                f.write_str(&format!(" {:w$}", c))?;
+            }
+            f.write_char('\n')?;
+        }
+        Ok(())
+    }
+}
+
+impl<const M: usize, const E: usize> std::ops::AddAssign for NumberOfState<M, E> {
+    fn add_assign(&mut self, rhs: Self) {
+        for (a, b) in self.data.iter_mut().zip(rhs.data) {
+            for (a, b) in a.iter_mut().zip(b) {
+                *a += b;
+            }
+        }
+    }
+}
+
+pub fn calc_2x2() -> NumberOfState<5, 9> {
+    let mut number_of_state = NumberOfState::new();
     for cell in 0u32..=(1 << 4) - 1 {
         let mag = cell.count_ones();
         let slide_y = cell >> 2 | cell << 2 & 0b11_00;
         let slide_x = cell >> 1 & 0b01_01 | cell << 1 & 0b10_10;
         let ene = (cell ^ slide_x).count_ones() + (cell ^ slide_y).count_ones();
-        number_of_state[ene as usize][mag as usize] += 1;
+        number_of_state.incr(ene, mag);
     }
     number_of_state
 }
 
-pub fn calc_3x3() -> [[u64; 10]; 19] {
-    let mut number_of_state = [[0; 10]; 19];
+pub fn calc_3x3() -> NumberOfState<10, 19> {
+    let mut number_of_state = NumberOfState::new();
     for cell in 0u32..=(1 << 9) - 1 {
         let mag = cell.count_ones();
         let slide_y = cell >> 3 | cell << 6 & 0b111_000_000;
         let slide_x = cell >> 1 & 0b011_011_011 | cell << 2 & 0b100_100_100;
         let ene = (cell ^ slide_x).count_ones() + (cell ^ slide_y).count_ones();
-        number_of_state[ene as usize][mag as usize] += 1;
+        number_of_state.incr(ene, mag);
     }
     number_of_state
 }
 
-pub fn calc_4x4() -> [[u64; 17]; 33] {
-    let mut number_of_state = [[0; 17]; 33];
+pub fn calc_4x4() -> NumberOfState<17, 33> {
+    let mut number_of_state = NumberOfState::new();
     for cell in 0u32..=(1 << 16) - 1 {
         let mag = cell.count_ones();
         let slide_y = cell >> 4 | cell << 12 & 0xf000;
         let slide_x = cell >> 1 & 0x7777 | cell << 3 & 0x8888;
         let ene = (cell ^ slide_x).count_ones() + (cell ^ slide_y).count_ones();
-        number_of_state[ene as usize][mag as usize] += 1;
+        number_of_state.incr(ene, mag);
     }
     number_of_state
 }
 
-pub fn calc_5x5() -> [[u64; 26]; 51] {
-    let mut number_of_state = [[0; 26]; 51];
+pub fn calc_5x5() -> NumberOfState<26, 51> {
+    let mut number_of_state = NumberOfState::new();
     for cell in 0u32..=(1 << 25) - 1 {
         let mag = cell.count_ones();
         let slide_y = cell >> 5 | cell << 20 & 0b11111 << 20;
         let slide_x = cell >> 1 & 0b01111_01111_01111_01111_01111
             | cell << 4 & 0b10000_10000_10000_10000_10000;
         let ene = (cell ^ slide_x).count_ones() + (cell ^ slide_y).count_ones();
-        number_of_state[ene as usize][mag as usize] += 1;
+        number_of_state.incr(ene, mag);
     }
     number_of_state
 }
 
-pub fn calc_6x6(threads: u64) -> [[u64; 37]; 73] {
-    fn calc(start: u64, end: u64) -> [[u64; 37]; 73] {
-        let mut number_of_state = [[0; 37]; 73];
+pub fn calc_6x6(threads: u64) -> NumberOfState<37, 73> {
+    fn calc(start: u64, end: u64) -> NumberOfState<37, 73> {
+        let mut number_of_state = NumberOfState::new();
         for cell in start..end {
             let mag = cell.count_ones();
             let slide_y = cell >> 6 | cell << 30 & 0b111111 << 30;
             let slide_x = cell >> 1 & 0b011111_011111_011111_011111_011111_011111
                 | cell << 5 & 0b100000_100000_100000_100000_100000_100000;
             let ene = (cell ^ slide_x).count_ones() + (cell ^ slide_y).count_ones();
-            number_of_state[ene as usize][mag as usize] += 1;
+            number_of_state.incr(ene, mag);
         }
         number_of_state
     }
@@ -102,12 +152,7 @@ pub fn calc_6x6(threads: u64) -> [[u64; 37]; 73] {
     }
     let mut number_of_state = calc(n / threads * (threads - 1), n);
     for h in handles {
-        let b = h.join().unwrap();
-        for (a, b) in number_of_state.iter_mut().zip(b) {
-            for (a, b) in a.iter_mut().zip(b) {
-                *a += b;
-            }
-        }
+        number_of_state += h.join().unwrap();
     }
     number_of_state
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use ising_partition_function::*;
 
-fn main() {
-    print_number_of_state(calc_2x2());
-    print_number_of_state(calc_3x3());
-    print_number_of_state(calc_4x4());
-    print_number_of_state(calc_5x5());
-    print_number_of_state(calc_6x6(6));
+fn main() -> std::io::Result<()> {
+    let n = calc_2x2();
+    n.save_file("data/2x2.txt")?;
+    println!("{}", n);
+
+    let n = calc_3x3();
+    n.save_file("data/3x3.txt")?;
+    println!("{}", n);
+
+    let n = calc_4x4();
+    n.save_file("data/4x4.txt")?;
+    println!("{}", n);
+
+    let n = calc_5x5();
+    n.save_file("data/5x5.txt")?;
+    println!("{}", n);
+
+    let n = calc_6x6(6);
+    n.save_file("data/6x6.txt")?;
+    println!("{}", n);
+    Ok(())
 }
